0931c5f5fed2a0a8ff3f3b78ebe2eeba83db044d
Lucas-Maia <lucas75maia@gmail.com>
2022-11-10 07:11:54 -0300

    ATV2

---
+++ ATV2.js
@@ -0,0 +1,12 @@
+//Atividade 2
+
+// 1- Se caminho 5Km e corro 2Km, passo o dia bem
+// caminho_5Km && corro_2Km, passo_o_dia_bem
+
+
+// 2- Se levanto cedo OU antes das 8h, saio tranquilo
+// levanto_cedo || antes_das8h, saio_tranquilo
+
+
+// 3- Se NÃO termino o trabalho OU NÃO tenho compromisso, saio tarde
+// ! termino_trablaho || ! tenho_compromisso, saio_tarde
