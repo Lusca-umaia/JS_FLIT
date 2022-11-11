e259d6428b49e9672d9952a3e4c5027de435b7e2
Lucas-Maia <lucas75maia@gmail.com>
2022-11-11 20:07:14 -0300

    Condicionais

---
+++ CONDI.js
@@ -0,0 +1,36 @@
+//Caso 1
+let caminho, corro
+caminho = "5Km"
+corro = "2Km"
+
+if (caminho === "5Km" && corro === "2Km") {
+    console.log("Passo o dia bem")
+}
+else {
+    console.log("Não passo o dia bem")
+}
+
+//Caso 2
+let levantarCedo, levantarAntes8h
+levantarCedo = true
+levantarAntes8h = false
+
+if (levantarCedo == true || levantarAntes8h == true) {
+    console.log("Saio tranquilo")
+}
+else {
+    console.log("Não saio tranquilo")
+}
+
+//Caso 3
+let TermTrabalho, compromisso
+
+TermTrabalho = true
+compromisso = false
+
+if (TermTrabalho != true || compromisso != true) {
+    console.log("Saio tarde")
+}
+else {
+    console.log("Não saio tarde")
+}
